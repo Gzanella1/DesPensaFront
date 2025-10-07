5da2ad7634329ee1c1644951202e044650a3eba3
Gzanella1 <giovanizanelladamaia123@gmail.com>
2025-10-07 10:52:21 -0300

    first commit

---
+++ scriptTemplete/carregarSidebar.js
@@ -0,0 +1,15 @@
+// carregarSidebar.js
+document.addEventListener("DOMContentLoaded", function () {
+    const container = document.getElementById("sidebar-container");
+    
+    fetch("sidebar.html")
+      .then(response => response.text())
+      .then(data => {
+        container.innerHTML = data;
+      })
+      .catch(error => {
+        console.error("Erro ao carregar a sidebar:", error);
+      });
+  });
+
+
