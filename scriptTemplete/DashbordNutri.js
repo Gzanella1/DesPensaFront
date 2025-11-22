1c1dbd7e638c91327b58be01816c07c67db34963
Giovani  Zanella da Maia <96748771+Gzanella1@users.noreply.github.com>
2025-11-21 22:09:07 -0300

    Revert "Revert "Merge pull request #14 from Gzanella1/carlos""

--- scriptTemplete/DashbordNutri.js
+++ scriptTemplete/DashbordNutri.js
@@ -11,8 +11,7 @@ function fecharFormulario() {
 }
  
 function limparFormulario() {
-  const inputs = document.querySelectorAll("#formInsumo input");
-  inputs.forEach(input => input.value = "");
+  document.querySelectorAll("#formInsumo input").forEach(i => i.value = "");
 }
  
 function confirmarFormulario() {
@@ -54,7 +53,7 @@ function visualizarEstoque() {
   container.appendChild(titulo);
  
   if (insumos.length === 0) {
-    container.innerHTML += `<p class="text-gray-500">Nenhum insumo cadastrado ainda.</p>`;
+    container.innerHTML = `<p class="text-gray-500">Nenhum insumo cadastrado ainda.</p>`;
     return;
   }
  
@@ -64,23 +63,24 @@ function visualizarEstoque() {
   const cabecalho = `
     <thead class="bg-green-dark text-white">
       <tr>
-        <th class="px-4 py-2 text-left">Tipo</th>
-        <th class="px-4 py-2 text-left">Endereço</th>
-        <th class="px-4 py-2 text-left">Instituição</th>
-        <th class="px-4 py-2 text-left">Quantidade / Validade</th>
-        <th class="px-4 py-2 text-left">Restrições</th>
+        <th class="px-2 py-2 text-left">Tipo</th>
+        <th class="px-2 py-2 text-left">Endereço</th>
+        <th class="px-1 py-2 text-left">Instituição</th>
+        <th class="px-3py-1text-left">Quantidade - Validade</th>
+        <th class="px-1 py-2 text-left">Restrições</th>
       </tr>
     </thead>
   `;
+
   let corpo = "<tbody class='divide-y divide-gray-200'>";
   insumos.forEach(item => {
     corpo += `
       <tr class="hover:bg-gray-50">
-        <td class="px-4 py-2">${item["tipo"] || "-"}</td>
-        <td class="px-4 py-2">${item["insira o endereço da propriedade"] || "-"}</td>
-        <td class="px-4 py-2">${item["lista de instituição"] || "-"}</td>
-        <td class="px-4 py-2">${item["Quantidade / Validade"] || "-"}</td>
-        <td class="px-4 py-2">${item["CheckList com as restrição"] || "-"}</td>
+        <td class="px-4 py-2">${item.tipo}</td>
+        <td class="px-4 py-2">${item.endereco}</td>
+        <td class="px-5 py-2">${item.instituicao}</td>
+        <td class="px-2 py-2">${item.quantidade} - ${item.validade}</td>
+        <td class="px-3 py-2">${item.checklist}</td>
       </tr>
     `;
   });
